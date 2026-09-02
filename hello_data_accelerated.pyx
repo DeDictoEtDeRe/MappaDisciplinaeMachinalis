ca97604db674549f55d7c09a5a8ea599044ee418
DeDictoEtDeRe <fuheric@proton.me>
2026-09-02 15:42:23 +0800

    Modify hello_data_accelerated.pyx line 9 and thereby might(subjonctiv) seminate to the lines of 18535 to 18572 of hello_data_accelerated.c for I shalln't assume the change would only exist in those confined lines because I didn't command to see der unterschied of priori und posteriori par patch

--- hello_data_accelerated.pyx
+++ hello_data_accelerated.pyx
@@ -6,7 +6,7 @@ import numpy as np
 def run_cython_accelerated():
     scores=pd.DataFrame({"name":["Ada","Line","Sam"],"score":[85,91,88]})
     #Extract raw numpy array and specify the view type for memory fetch speed
-    cdef long[:] score_view=scores['score'].values
+    cdef const long[:] score_view=scores['score'].values
     cdef int num_rows=score_view.shape[0]
     cdef long total_score=0
     cdef int i
